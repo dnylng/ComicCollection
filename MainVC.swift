b41db4e2b8e342e937c0c703fa69d68b91bdcb42
dnylng <iamdannyluong@gmail.com>
2017-07-14 15:59:16 -0400

    Set up FetchedResultsController stuff

--- MainVC.swift
+++ MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  ComicCollection
 //
 //  Created by Danny Luong on 7/13/17.
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import CoreData
 
-class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var segment: UISegmentedControl!
+    
+    var controller: NSFetchedResultsController<Item>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +33,79 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 0
+    }
+    
+    func attemptFetch() {
+        
+        // Fetch Item class
+        let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        // Sort data that is being fetched based on timestamp
+        let dateSort = NSSortDescriptor(key: "created", ascending: false)
+        fetchRequest.sortDescriptors = [dateSort]
+        
+        // Instantiate fetch result controller
+        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        do {
+            
+            try controller.performFetch()
+        } catch {
+            
+            let error = error as NSError
+            print("\(error)")
+        }
+        
+    }
+    
+    // Whenever tableView is about to update, it'll listen for changes
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    // When the controller did change, end the updates
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch(type) {
+            
+        case.insert:
+            if let indexPath = newIndexPath {
+                
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break
+            
+        case.delete:
+            if let indexPath = indexPath {
+                
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break
+            
+        case.update:
+            if let indexPath = indexPath {
+                
+                let cell = tableView.cellForRow(at: indexPath) as! ItemCell
+                
+                // Update the cell data
+            }
+            break
+            
+        case.move:
+            if let indexPath = indexPath {
+                
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            if let indexPath = newIndexPath {
+                
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break
+            
+        }
     }
     
 }
